d491b1aff8e364f99468f152ba5d58448a65a571
roseline124 <guseod24@gmail.com>
2019-09-18 10:05:26 +0900

    fix errors

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import time
 from selenium import webdriver
 import sys
 
 from settings import *
+from profile import *
+
 
 BASE_URL = f'https://{COMPANY_NAME}.daouoffice.com'
 LOGIN_URL = BASE_URL + '/login'
@@ -19,18 +22,15 @@ def is_login():
 
 def login(WAIT_SECONDS=2):
     driver.get(LOGIN_URL)
-    driver.implicitly_wait(WAIT_SECONDS)
+    time.sleep(WAIT_SECONDS)
 
-    input_ID = driver.find_element_by_name('username')
-    input_ID.send_keys(LOGIN_INFO['ID'])
-
-    input_PW = driver.find_element_by_name('password')
-    input_PW.send_keys(LOGIN_INFO['PASSWORD'])
+    driver.find_element_by_name('username').send_keys(LOGIN_INFO['ID'])
+    driver.find_element_by_name('password').send_keys(LOGIN_INFO['PASSWORD'])
 
     button_login = driver.find_element_by_xpath('//*[@id="login_submit"]')
     button_login.click()
 
-    driver.implicitly_wait(WAIT_SECONDS)
+    time.sleep(WAIT_SECONDS)
 
 
 ###########################
@@ -40,25 +40,22 @@ def login(WAIT_SECONDS=2):
 def is_checked(button):
     classes = button.get_attribute("class").split()
 
-    return True if 'on' in classes else False
+    return True if 'off' in classes else False
 
 
-def check(check_option):
+def check_attendance(check_option):
     driver.get(ATTEND_URL)
-    driver.implicitly_wait(WAIT_SECONDS)
+    time.sleep(WAIT_SECONDS)
 
     if check_option == 'in':
-        print(f'Hello, {Name}')
         button = driver.find_element_by_xpath('//*[@id="workIn"]')
     else:
-        print(f'Bye, {Name}')
         button = driver.find_element_by_xpath('//*[@id="workOut"]')
 
-    if not is_checked():
-        button.click()
-    else:
+    if is_checked(button):
         print("You already checked.")
-
+    else:
+        button.click()
 
 ###########################
 #  main
@@ -69,13 +66,15 @@ if __name__ == "__main__":
 
     if check_option not in ["in", "out"]:
         print("Please write : 'python main.py in' or 'python main.py out'")
-        return
+        sys.exit()
 
     driver = webdriver.Chrome(CHROMEDRIVER_PATH)
+
+    login()
 
     while not is_login():
         WAIT_SECONDS += 1
         login(WAIT_SECONDS=WAIT_SECONDS)
 
-    check(check_option)
+    check_attendance(check_option)
     driver.close()
